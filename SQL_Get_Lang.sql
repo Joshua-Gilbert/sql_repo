b0197123f8e9f780e4ffdb6d99d8f0ba618d86b1
Joshua-Gilbert <54961107+Joshua-Gilbert@users.noreply.github.com>
2019-10-31 13:33:48 -0700

    Update SQL_Get_Lang.sql

--- SQL_Get_Lang.sql
+++ SQL_Get_Lang.sql
@@ -2,4 +2,8 @@
 
 SELECT @@LANGUAGE AS Current_Language;  
 
+--sample output
+--Current_Language
+--us_english
+
 --source: https://www.c-sharpcorner.com/article/50-important-queries-in-sql-server/

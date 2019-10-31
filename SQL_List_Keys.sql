68a0dac1d220226782d4f5736745cd5668678171
Joshua-Gilbert <54961107+Joshua-Gilbert@users.noreply.github.com>
2019-10-31 13:36:01 -0700

    Update SQL_List_Keys.sql

--- SQL_List_Keys.sql
+++ SQL_List_Keys.sql
@@ -10,4 +10,12 @@ Table_Schema AS [Schema],
 Table_Name AS [TableName] FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE  
   
 GO
+
+--sample output World Wide Importers
+--Constraint	Schema	TableName
+--FK_Application_Cities_Application_People	Application	Cities
+--FK_Application_Cities_StateProvinceID_Application_StateProvinces	Application	Cities
+--FK_Application_Countries_Application_People	Application	Countries
+--FK_Application_DeliveryMethods_Application_People	Application	DeliveryMethods
+
 --Source: https://www.c-sharpcorner.com/article/50-important-queries-in-sql-server/

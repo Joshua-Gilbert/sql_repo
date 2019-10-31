86098f567b463bd1a8daf5efae54e38eb65248a1
Joshua-Gilbert <54961107+Joshua-Gilbert@users.noreply.github.com>
2019-10-31 14:12:58 -0700

    Update SQL_Get_All_Tables_With_View.sql

--- SQL_Get_All_Tables_With_View.sql
+++ SQL_Get_All_Tables_With_View.sql
@@ -1,1 +1,7 @@
 SELECT * FROM INFORMATION_SCHEMA.VIEW_TABLE_USAGE  
+-- Sample Output
+--VIEW_CATALOG	VIEW_SCHEMA	VIEW_NAME	TABLE_CATALOG	TABLE_SCHEMA	TABLE_NAME
+--WideWorldImporters	Website	Customers	WideWorldImporters	Application	Cities
+--WideWorldImporters	Website	Customers	WideWorldImporters	Application	DeliveryMethods
+--WideWorldImporters	Website	Customers	WideWorldImporters	Application	People
+--Source:https://www.c-sharpcorner.com/article/50-important-queries-in-sql-server/

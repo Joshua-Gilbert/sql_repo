551bc08c2cde0c11d665551356becdefadefa808
Joshua-Gilbert <54961107+Joshua-Gilbert@users.noreply.github.com>
2019-10-31 13:39:29 -0700

    Update SQL_Version_Check.sql

--- SQL_Version_Check.sql
+++ SQL_Version_Check.sql
@@ -1,3 +1,7 @@
 --Checks the version of SQL that's running
-SELECT @@VERSION AS Version_Name  
+SELECT @@VERSION AS Version_Name
+-- sample output
+--Version_Name
+--Microsoft SQL Server 2017 (RTM) - 14.0.1000.169 (X64)   Aug 22 2017 17:04:49   Copyright (C) 2017 Microsoft Corporation  
+--Express Edition (64-bit) on Windows 10 Home 10.0 <X64> (Build 18362: ) 
 --Source:https://www.c-sharpcorner.com/article/50-important-queries-in-sql-server/

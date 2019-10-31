47c89bc9625724d88be9f8142154958b182956a5
Joshua-Gilbert <54961107+Joshua-Gilbert@users.noreply.github.com>
2019-10-31 13:37:36 -0700

    Update SQL_Return_Server_Name.sql

--- SQL_Return_Server_Name.sql
+++ SQL_Return_Server_Name.sql
@@ -1,3 +1,6 @@
 --SQL query to return the name of the SQL server
 SELECT @@SERVERNAME AS 'Server_Name'
+-- sample output
+--Server_Name
+--LAPTOP-CQSDQNFC\SQLEXPRESS
 --source: https://www.c-sharpcorner.com/article/50-important-queries-in-sql-server/

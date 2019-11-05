3ecf755700a6e2dd0b0e04300959cdba673236d9
Joshua-Gilbert <54961107+Joshua-Gilbert@users.noreply.github.com>
2019-11-05 12:47:24 -0800

    Add files via upload
    Returns free space on all drives
    Source: https://www.c-sharpcorner.com/article/50-important-queries-in-sql-server/

---
+++ SQL_Get_Harddrive_Space.sql
@@ -0,0 +1,7 @@
+--Gets free space on drives
+EXEC master..xp_fixeddrives 
+--source:https://www.c-sharpcorner.com/article/50-important-queries-in-sql-server/ 
+--sample output:
+--drive	MB free
+--C	13002
+--D	1723

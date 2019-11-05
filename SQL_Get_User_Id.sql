dd358a812124218a3486f8456f3fe5adcc24782c
Joshua-Gilbert <54961107+Joshua-Gilbert@users.noreply.github.com>
2019-11-05 13:07:12 -0800

    Add files via upload
    Returns Current User ID
    source: https://www.c-sharpcorner.com/article/50-important-queries-in-sql-server/

---
+++ SQL_Get_User_Id.sql
@@ -0,0 +1,6 @@
+--Gets Current user Session Id
+SELECT @@SPID AS 'Session_Id'
+--sample output:
+--Session_Id
+--51
+--source:https://www.c-sharpcorner.com/article/50-important-queries-in-sql-server/

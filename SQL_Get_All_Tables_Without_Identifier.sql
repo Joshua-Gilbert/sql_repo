37ee5ef2b94f43d57de8724106c8d48afb851760
Joshua-Gilbert <54961107+Joshua-Gilbert@users.noreply.github.com>
2019-11-19 13:54:53 -0800

    Update SQL_Get_All_Tables_Without_Identifier.sql

--- SQL_Get_All_Tables_Without_Identifier.sql
+++ SQL_Get_All_Tables_Without_Identifier.sql
@@ -7,3 +7,14 @@ FROM sys.tables
 WHERE OBJECTPROPERTY(OBJECT_ID,'TableHasIdentity') = 0  
   
 ORDER BY Table_Name;  
+
+--Sample Output
+--Table_Name
+--MSreplication_options
+--spt_fallback_db
+--spt_fallback_dev
+--spt_fallback_usg
+--spt_monitor
+
+
+--Source:https://www.c-sharpcorner.com/article/50-important-queries-in-sql-server/

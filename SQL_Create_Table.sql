3c3552cc2d6e89809291bdda78949a960edbf1c0
Joshua-Gilbert <54961107+Joshua-Gilbert@users.noreply.github.com>
2019-12-05 14:19:11 -0800

    Add files via upload
    SQL Command to create a new table
    Source: https://www.c-sharpcorner.com/article/top-10-mostly-used-sql-queries/

---
+++ SQL_Create_Table.sql
@@ -0,0 +1,11 @@
+--SQL Command to create table
+
+CREATE TABLE student  
+(  
+    id INTEGER PRIMARY KEY,  
+    name varchar(20),  
+    address varchar(50),  
+    age INTEGER  
+);  
+
+--Source:https://www.c-sharpcorner.com/article/top-10-mostly-used-sql-queries/

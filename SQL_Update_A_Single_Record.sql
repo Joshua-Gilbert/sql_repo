b1d3303d090b398305ede5c3cb5996c514392528
Joshua-Gilbert <54961107+Joshua-Gilbert@users.noreply.github.com>
2019-12-09 15:10:26 -0800

    Add files via upload
    Query to update a record on a table
    Source:http://www.codingfusion.com/Post/75-Important-queries-in-SQL-Server-every-developer-should-know

---
+++ SQL_Update_A_Single_Record.sql
@@ -0,0 +1,3 @@
+--Updates a record from a table
+UPDATE TableName SET NAME='Max Payne' WHERE Id=1
+--Source:http://www.codingfusion.com/Post/75-Important-queries-in-SQL-Server-every-developer-should-know

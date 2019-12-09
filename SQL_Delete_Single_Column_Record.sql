b301351513e220bd30282d10b272c63c0417faf6
Joshua-Gilbert <54961107+Joshua-Gilbert@users.noreply.github.com>
2019-12-09 15:31:56 -0800

    Add files via upload
    Query that will delete a single specified column record
    Source:http://www.codingfusion.com/Post/75-Important-queries-in-SQL-Server-every-developer-should-know

---
+++ SQL_Delete_Single_Column_Record.sql
@@ -0,0 +1,3 @@
+--SQL Query that will delete a single specified column record
+DELETE FROM TableName WHERE Id=1
+--Source:http://www.codingfusion.com/Post/75-Important-queries-in-SQL-Server-every-developer-should-know

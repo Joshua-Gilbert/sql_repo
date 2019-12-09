45b6598b971cfd848b4ab809fcf502b1490f0dfa
Joshua-Gilbert <54961107+Joshua-Gilbert@users.noreply.github.com>
2019-12-09 14:50:06 -0800

    Update SQL_Insert_Multiple_Rows.sql

--- SQL_Insert_Multiple_Rows.sql
+++ SQL_Insert_Multiple_Rows.sql
@@ -1,3 +1,4 @@
 --This query inserts multiple rows into a given table
 INSERT INTO TableName (Name,Age) 
 VALUES ('Max',30),('John',28),('Jack',31);
+--Source:http://www.codingfusion.com/Post/75-Important-queries-in-SQL-Server-every-developer-should-know

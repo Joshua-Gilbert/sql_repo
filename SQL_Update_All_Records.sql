29797d3266e48d274d5e17a6fbbf67cc04a18613
Joshua-Gilbert <54961107+Joshua-Gilbert@users.noreply.github.com>
2019-12-09 15:22:17 -0800

    Add files via upload
    Query that will completely set a single column a specified value.
    Source:http://www.codingfusion.com/Post/75-Important-queries-in-SQL-Server-every-developer-should-know

---
+++ SQL_Update_All_Records.sql
@@ -0,0 +1,3 @@
+--Updates all records on a column from a table
+UPDATE TableName SET ColumnName=31
+--Source:http://www.codingfusion.com/Post/75-Important-queries-in-SQL-Server-every-developer-should-know

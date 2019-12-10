a169f18f0e269377c475ea4c9fd4e295de667fa7
Joshua-Gilbert <54961107+Joshua-Gilbert@users.noreply.github.com>
2019-12-09 16:46:51 -0800

    Add files via upload
    Query that creates a procedure. Handy method for quick scripting.
    Source:http://www.codingfusion.com/Post/75-Important-queries-in-SQL-Server-every-developer-should-know

---
+++ SQL_Create_Stored_Procedure.sql
@@ -0,0 +1,5 @@
+--SQL Query that creates a sample stored procedure
+CREATE PROCEDURE getInfoFromTable
+AS
+SELECT * FROM TableName
+--Source:http://www.codingfusion.com/Post/75-Important-queries-in-SQL-Server-every-developer-should-know

88cb71e63de36cc04f0065fa11ee698b0eda5736
Joshua-Gilbert <54961107+Joshua-Gilbert@users.noreply.github.com>
2019-12-10 17:01:30 -0800

    Update SQL_Record_Date_Search.sql

--- SQL_Record_Date_Search.sql
+++ SQL_Record_Date_Search.sql
@@ -2,12 +2,18 @@
 
 SELECT * FROM Sales.Customers  WHERE CONVERT(date, Sales.Customers.AccountOpenedDate) BETWEEN '1/1/2015' AND '12/31/2016' 
 
---CustomerID	CustomerName	BillToCustomerID	CustomerCategoryID	BuyingGroupID	PrimaryContactPersonID	AlternateContactPersonID	DeliveryMethodID	DeliveryCityID	PostalCityID	CreditLimit	AccountOpenedDate	StandardDiscountPercentage	IsStatementSent	IsOnCreditHold	PaymentDays	PhoneNumber	FaxNumber	DeliveryRun	RunPosition	WebsiteURL	DeliveryAddressLine1	DeliveryAddressLine2	DeliveryPostalCode	DeliveryLocation	PostalAddressLine1	PostalAddressLine2	PostalPostalCode	LastEditedBy	ValidFrom	ValidTo
-1039	Bhaamini Palagummi	1039	6	NULL	3239	NULL	3	18410	18410	2300.00	2015-01-13	0.000	0	0	7	(212) 555-0100	(212) 555-0101	NULL	NULL	http://www.microsoft.com/	Unit 29	1035 Clarissa Avenue	90459	0xE6100000010C76E91505B0A844404436EBE7287652C0	PO Box 1576	Dayaramville	90459	1	2015-01-13 00:00:00.0000000	9999-12-31 23:59:59.9999999
-1040	Damodar Shenoy	1040	3	NULL	3240	NULL	3	30865	30865	2400.00	2015-01-16	0.000	0	0	7	(229) 555-0100	(229) 555-0101	NULL	NULL	http://www.microsoft.com/	Suite 7	1337 Koknesis Avenue	90673	0xE6100000010CEEEE5C73A2464040485AE0E1ACAA54C0	PO Box 5020	Annetteville	90673	1	2015-01-16 00:00:00.0000000	9999-12-31 23:59:59.9999999
-1041	Tomo Vidovic	1041	4	NULL	3241	NULL	3	34371	34371	2900.00	2015-01-18	0.000	0	0	7	(207) 555-0100	(207) 555-0101	NULL	NULL	http://www.microsoft.com/	Shop 14	296 Anima Boulevard	90085	0xE6100000010C706880C124224640170335C6981A51C0	PO Box 249	Anujville	90085	1	2015-01-18 00:00:00.0000000	9999-12-31 23:59:59.9999999
-1042	Nguyen Banh	1042	3	NULL	3242	NULL	3	11696	11696	2500.00	2015-01-22	0.000	0	0	7	(239) 555-0100	(239) 555-0101	NULL	NULL	http://www.microsoft.com/	Unit 7	1885 William Boulevard	90744	0xE6100000010C13526D2695BB3D4007FAFA108A7854C0	PO Box 8476	Bhanuville	90744	1	2015-01-22 00:00:00.0000000	9999-12-31 23:59:59.9999999
-1043	Raj Verma	1043	7	NULL	3243	NULL	3	7113	7113	3500.00	2015-01-30	0.000	0	0	7	(203) 555-0100	(203) 555-0101	NULL	NULL	http://www.microsoft.com/	Shop 9	678 Sang Crescent	90799	0xE6100000010CC10F1835A9FE4440A3186A5E1F4652C0	PO Box 5963	Prasertville	90799	1	2015-01-30 00:00:00.0000000	9999-12-31 23:59:59.9999999
+--CustomerID	CustomerName	BillToCustomerID	CustomerCategoryID	BuyingGroupID	PrimaryContactPersonID	AlternateContactPersonID	
+--DeliveryMethodID	DeliveryCityID	PostalCityID	CreditLimit	AccountOpenedDate	StandardDiscountPercentage	IsStatementSent	IsOnCreditHold	
+--PaymentDays	PhoneNumber	FaxNumber	DeliveryRun	RunPosition	WebsiteURL	DeliveryAddressLine1	DeliveryAddressLine2	DeliveryPostalCode	
+--DeliveryLocation	PostalAddressLine1	PostalAddressLine2	PostalPostalCode	LastEditedBy	ValidFrom	ValidTo
+
+--1039	Bhaamini Palagummi	1039	6	NULL	3239	NULL	3	18410	18410	2300.00	2015-01-13	0.000	0	0	7	(212) 555-0100	(212) 555-0101	NULL	
+--NULL	http://www.microsoft.com/	Unit 29	1035 Clarissa Avenue	90459	0xE6100000010C76E91505B0A844404436EBE7287652C0	PO Box 1576	
+--Dayaramville	90459	1	2015-01-13 00:00:00.0000000	9999-12-31 23:59:59.9999999
+
+--1040	Damodar Shenoy	1040	3	NULL	3240	NULL	3	30865	30865	2400.00	2015-01-16	0.000	0	0	7	(229) 555-0100	(229) 555-0101	
+--NULL	NULL	http://www.microsoft.com/	Suite 7	1337 Koknesis Avenue	90673	0xE6100000010CEEEE5C73A2464040485AE0E1ACAA54C0	PO Box 5020	
+--Annetteville	90673	1	2015-01-16 00:00:00.0000000	9999-12-31 23:59:59.9999999
 
 --Modified from:http://www.codingfusion.com/Post/75-Important-queries-in-SQL-Server-every-developer-should-know
 --for World Wide Importers Database

51350ad7b4ad916bc75599e37fb3ad58a58b2151
Joshua-Gilbert <54961107+Joshua-Gilbert@users.noreply.github.com>
2019-12-10 16:41:21 -0800

    Update SQL_Search_Column_Name.sql

--- SQL_Search_Column_Name.sql
+++ SQL_Search_Column_Name.sql
@@ -1,6 +1,16 @@
 --SQL Query that will allow a search of any specified column name in the database
+
 SELECT c.name AS ColName, t.name AS TableName
 FROM sys.columns c
 JOIN sys.tables t ON c.object_id = t.object_id
-WHERE c.name LIKE '%columnNameToSearch%'       
+WHERE c.name LIKE '%columnNameToSearch%'
+
+--Sample Output
+--ColName	TableName
+--SalesTerritory	StateProvinces
+--SalesTerritory	StateProvinces_Archive
+--SalespersonPersonID	Orders
+--IsSalesperson	People
+--IsSalesperson	People_Archive
+
 --Source:http://www.codingfusion.com/Post/75-Important-queries-in-SQL-Server-every-developer-should-know

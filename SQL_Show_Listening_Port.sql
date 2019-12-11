c4608a6fb1e3f0fcbcd22cbfba00fa9545d0a2d7
Joshua-Gilbert <54961107+Joshua-Gilbert@users.noreply.github.com>
2019-12-11 10:45:34 -0800

    Update SQL_Show_Listening_Port.sql

--- SQL_Show_Listening_Port.sql
+++ SQL_Show_Listening_Port.sql
@@ -5,6 +5,20 @@ FROM   sys.dm_exec_connections
 WHERE  session_id = @@SPID
 GO   
 
+--Alt Win Reg Command------------------------------------------------------------
+
+DECLARE       @portNumber   NVARCHAR(10)
+ 
+EXEC   xp_instance_regread
+@rootkey    = 'HKEY_LOCAL_MACHINE',
+@key        =
+'Software\Microsoft\Microsoft SQL Server\MSSQLServer\SuperSocketNetLib\Tcp\IpAll',
+@value_name = 'TcpDynamicPorts',
+@value      = @portNumber OUTPUT
+ 
+SELECT [Port Number] = @portNumber
+GO
+
 --Sample Output
 --Result Set:
 --local_tcp_port

864a56c942c2f4145a5a65557933d7dfd048bc26
Joshua-Gilbert <54961107+Joshua-Gilbert@users.noreply.github.com>
2019-12-11 10:52:34 -0800

    Update SQL_Bat.bat

--- SQL_Bat.bat
+++ SQL_Bat.bat
@@ -2,3 +2,9 @@
 CD C:\SQLite\
 %PATH%
 START sqlite3.exe
+--------------------------------------------
+--Alt for Microsoft SQL Server
+@Echo Off
+FOR /f %%i IN ('DIR *.Sql /B') do call :RunScript %%i
+GOTO :END
+Source:https://sqlandme.com/category/undocumented-functions/

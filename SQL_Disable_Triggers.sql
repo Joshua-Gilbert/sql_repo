b72e264613460a4c2f9b2a4d14272df95a99b267
Joshua-Gilbert <54961107+Joshua-Gilbert@users.noreply.github.com>
2019-12-13 13:10:15 -0800

    Add files via upload
    Query to disable triggers
    Source: http://www.codingfusion.com/Post/75-Important-queries-in-SQL-Server-every-developer-should-know

---
+++ SQL_Disable_Triggers.sql
@@ -0,0 +1,7 @@
+--SQL Query that will disable triggers in DB
+ALTER TABLE TableName DISABLE TRIGGER TriggerName  
+
+--Disable all triggers
+--ALTER TABLE TableName DISABLE TRIGGER ALL       
+
+--Source:http://www.codingfusion.com/Post/75-Important-queries-in-SQL-Server-every-developer-should-know

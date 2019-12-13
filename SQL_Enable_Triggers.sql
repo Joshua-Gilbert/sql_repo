1b97d3a1bbfacc61b0a27c4325a1cfbd77890a07
Joshua-Gilbert <54961107+Joshua-Gilbert@users.noreply.github.com>
2019-12-13 13:09:10 -0800

    Add files via upload
    Query to enable triggers
    Source: http://www.codingfusion.com/Post/75-Important-queries-in-SQL-Server-every-developer-should-know

---
+++ SQL_Enable_Triggers.sql
@@ -0,0 +1,8 @@
+--SQL Query that will enable triggers in DB
+
+--Enable particular trigger
+ALTER TABLE TableName ENABBLE TRIGGER TriggerName  
+--Enable all triggers
+--ALTER TABLE TableName ENABLE TRIGGER ALL
+        
+--Source:http://www.codingfusion.com/Post/75-Important-queries-in-SQL-Server-every-developer-should-know

f2f53552a4bcd51bf9846c833e5e13a3cb524eec
Joshua-Gilbert <54961107+Joshua-Gilbert@users.noreply.github.com>
2019-12-13 12:48:19 -0800

    Add files via upload
    Query that allows the user to rename a table
    Source: http://www.codingfusion.com/Post/75-Important-queries-in-SQL-Server-every-developer-should-know

---
+++ SQL_Rename_Table.sql
@@ -0,0 +1,5 @@
+--SQL Query that will allow a user to rename a table
+
+EXEC sp_rename 'OldTableName', 'NewTableName'
+
+--Source:http://www.codingfusion.com/Post/75-Important-queries-in-SQL-Server-every-developer-should-know

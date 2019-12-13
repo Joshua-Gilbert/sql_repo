3e8ec15610a431323612540ade2ebe47b0fd7be2
Joshua-Gilbert <54961107+Joshua-Gilbert@users.noreply.github.com>
2019-12-13 11:37:37 -0800

    Add files via upload
    Query that will return all the database names
    Source: http://www.codingfusion.com/Post/75-Important-queries-in-SQL-Server-every-developer-should-know

---
+++ SQL_Return_All_Database_Names.sql
@@ -0,0 +1,12 @@
+--SQL Query that will return all database names
+
+SELECT Name FROM dbo.sysdatabases      
+
+--Sample Output:Name
+--master
+--tempdb
+--model
+--msdb
+--WideWorldImporters
+
+--Source:http://www.codingfusion.com/Post/75-Important-queries-in-SQL-Server-every-developer-should-know
